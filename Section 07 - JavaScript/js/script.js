db0616b652765779f61db7754ce6ac831a4df787
SarahPri <princesswangari09@gmail.com>
2024-08-15 17:36:21 +0300

    JavaScript

---
+++ Section 07 - JavaScript/js/script.js
@@ -0,0 +1,22 @@
+function getMilk(packets) {
+  console.log('leaveHouse');
+  console.log('moveRight');
+  console.log('moveRight');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveRight');
+  console.log('moveRight');
+  console.log('buy ' + packets + ' packets of Milk');
+  console.log('moveLeft');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveLeft');
+  console.log('moveLeft');
+  console.log('enterHouse');
+}
+
+getMilk(2);
