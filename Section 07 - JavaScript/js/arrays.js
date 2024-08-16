b2f74469e1ba526e9faccc204f8ef8543958c716
SarahPri <princesswangari09@gmail.com>
2024-08-16 12:26:58 +0300

    JavaScript

---
+++ Section 07 - JavaScript/js/arrays.js
@@ -0,0 +1,8 @@
+let guestList = ["Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+
+let user = prompt("What is your name? ");
+if (guestList.includes === user) {
+  console.log(`Hi, ${user} You are invited`);
+} else {
+  console.log(`Hi, ${user} you are not in the list`);
+}
