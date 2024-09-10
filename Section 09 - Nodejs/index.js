ca94b2127bbe40bd52172a0e6323338f72e44f1b
SarahPri <princesswangari09@gmail.com>
2024-09-10 12:12:46 +0300

    Nodejs

---
+++ Section 09 - Nodejs/index.js
@@ -0,0 +1,16 @@
+// import superheroes from "superheroes";
+
+// var mySuperheroName = superheroes.random();
+// console.log(mySuperheroName);
+
+// const superheroes = require('superheroes');
+
+// // Get a random superhero name
+// const randomSuperhero = superheroes.random();
+
+// console.log(randomSuperhero);
+
+import superheroes from "superheroes";
+
+const randomSuperhero = superheroes.random();
+console.log(randomSuperhero);
