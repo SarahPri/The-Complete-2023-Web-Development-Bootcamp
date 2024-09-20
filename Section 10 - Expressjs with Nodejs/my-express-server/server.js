8a6871bea8071f3f07b572d2af77e23a27d26aae
SarahPri <princesswangari09@gmail.com>
2024-09-20 06:37:23 +0300

    changed

--- Section 10 - Expressjs with Nodejs/my-express-server/server.js
+++ Section 10 - Expressjs with Nodejs/my-express-server/server.js
@@ -1,5 +1,24 @@
-const express = require("express")
-const app = express()
-app.listen(3000, function(){
-  console.log('Server started on port 3000');
-})
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello Sarah</h1>");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Contact Us Page");
+});
+
+app.get("/about", function (req, res) {
+  res.send("About Page");
+});
+
+
+app.get("/hobbies", function (req, res) {
+  res.send("Hobbies Page");
+});
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
