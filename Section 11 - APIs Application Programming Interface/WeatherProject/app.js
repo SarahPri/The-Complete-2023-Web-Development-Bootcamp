466d3202be64f3c2c9057e0d26ed639170dbd2ef
SarahPri <princesswangari09@gmail.com>
2024-09-21 14:08:52 +0300

    Weather App

--- Section 11 - APIs Application Programming Interface/WeatherProject/app.js
+++ Section 11 - APIs Application Programming Interface/WeatherProject/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true })); //passing through the text
 
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "8f8f93d3e145ff5f4251e8dfe11fb626";
+  const unit = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Nakuru&appid=8f8f93d3e145ff5f4251e8dfe11fb626&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -24,13 +38,16 @@ app.get("/", function (req, res) {
 
       const icon = weatherData.weather[0].icon;
 
-      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-      
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
       res.write("<h1>The weather is currently  " + description + "</h1>");
 
       res.write(
-        "<h2> The temperature in Nakuru is " + temp + " degrees Celcius. </h2>"
+        "<h2> The temperature in " +
+          query +
+          " is " +
+          temp +
+          " degrees Celcius. </h2>"
       );
 
       res.write("<h3>The pressure today is " + pressure + "</h3>");
