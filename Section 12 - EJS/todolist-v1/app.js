b5d834087e48fca78e04a6c0f60bb44cc837bd43
SarahPri <princesswangari09@gmail.com>
2024-09-27 01:45:41 +0300

    templating using ejs

--- Section 12 - EJS/todolist-v1/app.js
+++ Section 12 - EJS/todolist-v1/app.js
@@ -2,17 +2,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  if (today.getDay() === 6  || today.getDay() === 0) {
-    res.send("Yuuupiiiii it's the weekend!");
-  } else {
-    res.send("Maan I have to work!");
+  var currentDay = today.getDay();
+  var day = "";
+
+  // if (currentDay === 6 || currentDay === 0) {
+  //   day = "weekend";
+  // } else {
+  //   day = "weekday";
+  // }
+
+  switch(currentDay){
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+      
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function (req, res) {
   console.log("The server is running on port 3000");
 });
- 
